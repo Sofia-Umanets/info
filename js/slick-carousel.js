ade683844dd50e70000d2cfaa839c953cc89b42b
Sofia-Umanets <sonatennis2006@gmail.com>
2025-04-15 00:15:01 +0300

    Update slick-carousel.js

--- js/slick-carousel.js
+++ js/slick-carousel.js
@@ -1,23 +1,18 @@
-$(document).ready(function(){
-    // Ждем 500мс для гарантии загрузки всех компонентов
-    setTimeout(function() {
-        $(".slider").slick({
-            arrows: true,
-            dots: true,
-            infinite: true,
-            
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToScroll: 1,
-                        slidesToShow: 1
-                    }
+$(window).on('load', function() {
+    $(".slider").slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        slidesToScroll: 3,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1
                 }
-            ],
-            
-            slidesToScroll: 3,
-            slidesToShow: 3
-        });
-    }, 500);
+            }
+        ]
+    });
 });
